d95626e0c211d80ad7d9b5ec3f7c357dbc96aa73
Qipei Mei <qipei@ualberta.ca>
2020-05-07 06:01:56 -0600

    remove a space

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import jwt
 # pylint: disable=import-error
 from flask import Flask, jsonify, request, abort
 
-
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
